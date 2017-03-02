c61dcfd63ca59b10d8ae5d419c4005504b355152
Ijeoma Arisah <ijeomarisah@gmail.com>
2017-03-02 19:25:35 +0100

    add validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
   has_many :tickets
 
   validates :first_name, :last_name, :email, presence: true
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
+                    uniqueness: {case_sensitive: false, message: 'This email address already exists'}
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :admin
+  attributes :id, :email, :first_name, :type, :last_name, :admin
   has_many :tickets
 end
