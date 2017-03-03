8822f00eb6e0d9355a56d63bbf7e414c8a407ad3
Ijeoma Arisah <ijeomarisah@gmail.com>
2017-03-03 22:32:24 +0100

    add seeded file for admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+email = ENV['admin_email']
+password = ENV['admin_password']
+first_name = ENV['admin_first_name']
+last_name = ENV['admin_last_name']
+
+puts "Hi, #{email}!"
+
+puts 'I am creating you as an admin user...'
+
+admin_user = SupportAgent.find_or_create_by(email: email) do |user|
+  user.password = password
+  user.first_name = first_name
+  user.last_name = last_name
+  user.admin = true
+end
+
+if admin_user.save
+  puts "You have been created as an admin user, #{admin_user.first_name} #{admin_user.last_name}"
+else
+  puts 'error saving admin'
+end
